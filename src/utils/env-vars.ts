d3f624ccb353a85a7f3e3fcd7d1858f4001a1d2e
Renato <renatoperezc@gmail.com>
2023-04-03 20:05:42 -0600

    Use the Next.js framework env var for vercel url

--- src/utils/env-vars.ts
+++ src/utils/env-vars.ts
@@ -2,5 +2,5 @@ export const flexhireBaseUrl = process.env.FLEXHIRE_API_BASE_URL;
 export const baseUrl =
   process.env.NODE_ENV === "development"
     ? process.env.NEXT_PUBLIC_BASE_URL!
-    : `https://${process.env.NEXT_PUBLIC_BASE_URL_VERCEL!}`;
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL!}`;
 export const flexhireApiKey = process.env.FLEXHIRE_API_KEY;
