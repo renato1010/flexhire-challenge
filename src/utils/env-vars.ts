c04dbe0096cfc1bdb59f035f9ed2803cb93488aa
Renato <renatoperezc@gmail.com>
2023-04-03 19:36:13 -0600

    Set env var vercel url available to browser

--- src/utils/env-vars.ts
+++ src/utils/env-vars.ts
@@ -2,5 +2,5 @@ export const flexhireBaseUrl = process.env.FLEXHIRE_API_BASE_URL;
 export const baseUrl =
   process.env.NODE_ENV === "development"
     ? process.env.NEXT_PUBLIC_BASE_URL!
-    : `https://${process.env.VERCEL_URL!}`;
+    : `https://${process.env.NEXT_PUBLIC_BASE_URL_VERCEL!}`;
 export const flexhireApiKey = process.env.FLEXHIRE_API_KEY;
